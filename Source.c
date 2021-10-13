e31bfc3ff5253411a6d13208f17b123bea531968
Ryan Ries <ryanries09@gmail.com>
2021-10-12 20:28:25 -0500

    Add project files.

---
+++ Source.c
@@ -0,0 +1,257 @@
+// Joseph Ryan Ries, 2021
+// It sucks that I have to do this, but Windows 11 at this time does not
+// allow us to display the clock on other monitors other than the primary
+// monitor. It is very important to me to be able to display the clock on
+// monitors other than the primary one.
+// I currently only have two monitors to test with so this app only works 
+// on 1 additional non-primary monitor. I'm happy to take pull requests 
+// if you want to add support for multiple non-primary monitors.
+// Also I haven't added logic for taskbar on the left or right or top of the monitor.
+// IMPORTANT: "Per-monitor DPI Awareness" is set in the application's manifest!
+
+#include <windows.h>
+
+#include <stdio.h>
+
+#define APP_NAME_W L"JustAClock"
+
+#define CLOCK_WINDOW_WIDTH	80
+
+volatile BOOL gShouldExit;
+
+MONITORINFO gMonitorInfo = { sizeof(MONITORINFO) };
+
+
+// Callback function called by EnumDisplayMonitors for each enabled monitor.
+// Since EnumDisplayMonitors
+// WARNING: CURRENTLY ONLY SUPPORTS ONE EXTRA MONITOR!
+BOOL CALLBACK EnumDisplayProc(HMONITOR hMon, HDC dcMon, RECT* pRcMon, LPARAM Parameter)
+{
+	// Raymond Chen says: "The primary monitor by definition has its upper left corner at (0, 0)."
+	if ((pRcMon->left != 0) && (pRcMon->top != 0))
+	{
+		// We found a secondary monitor!
+		// We need to position our clock window at the bottom right corner of this monitor's taskbar.		
+
+		if (GetMonitorInfoW(hMon, &gMonitorInfo) == 0)
+		{
+			MessageBoxW(NULL, L"GetMonitorInfoW() failed!", L"ERROR", MB_ICONERROR | MB_OK);
+
+			gShouldExit = TRUE;
+		}		
+	}
+
+	return(TRUE);
+}
+
+LRESULT CALLBACK ClockWindowProc(_In_ HWND WindowHandle, _In_ UINT Message, _In_ WPARAM WParam, _In_ LPARAM LParam)
+{
+	LRESULT Result = 0;
+
+	switch (Message)
+	{		
+		case WM_CLOSE:
+		{
+			PostQuitMessage(0);
+
+			gShouldExit = TRUE;
+
+			break;
+		}
+		case WM_PAINT:
+		{
+			SYSTEMTIME Time = { 0 };
+
+			wchar_t DateTimeString[32] = { 0 };			
+
+			RECT ClientRect = { 0 };
+
+			PAINTSTRUCT PaintStruct = { 0 };
+
+			HDC Context = BeginPaint(WindowHandle, &PaintStruct);
+
+			GetClientRect(WindowHandle, &ClientRect);
+
+			ClientRect.top += 10;
+
+			ClientRect.right -= 8;
+
+			SetTextColor(Context, RGB(255, 255, 255));
+
+			SetBkMode(Context, TRANSPARENT);
+
+			HFONT hFont = GetStockObject(DEFAULT_GUI_FONT);
+			
+			LOGFONT logfont;
+
+			GetObjectW(hFont, sizeof(LOGFONT), &logfont);
+
+			// Now change the logfont.lfHeight member
+
+			HFONT hNewFont = CreateFontIndirectW(&logfont);
+
+			HFONT hOldFont = (HFONT)SelectObject(Context, hNewFont);
+
+			GetLocalTime(&Time);
+			
+			_snwprintf_s(
+				DateTimeString, 
+				_countof(DateTimeString), 
+				_TRUNCATE, 
+				L"%02d:%02d:%02d\n%d/%d/%d", 
+				Time.wHour, Time.wMinute, Time.wSecond, Time.wMonth, Time.wDay, Time.wYear);
+
+			DrawTextW(Context, DateTimeString, -1, &ClientRect, DT_NOCLIP | DT_RIGHT);
+
+			// Always select the old font back into the DC
+			SelectObject(Context, hOldFont);
+
+			DeleteObject(hNewFont);
+
+			EndPaint(WindowHandle, &PaintStruct);
+
+			break;
+		}
+		default:
+		{
+			Result = DefWindowProcW(WindowHandle, Message, WParam, LParam);
+		}
+	}
+
+	return(Result);
+}
+
+// The application's entry point.
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
+{
+	DWORD Result = ERROR_SUCCESS;	
+	
+	MSG WindowMessage = { 0 };
+
+	HWND ClockWindowHandle = NULL;
+
+	WNDCLASSEXW WindowClass = { sizeof(WNDCLASSEXW) };	
+
+	WindowClass.style = 0;
+
+	WindowClass.lpfnWndProc = ClockWindowProc;
+
+	// We will make red pixels transparent, effectively making the window transparent.
+	WindowClass.hbrBackground = CreateSolidBrush(RGB(255, 0, 0));
+
+	WindowClass.cbClsExtra = 0;
+
+	WindowClass.cbWndExtra = 0;
+
+	WindowClass.hInstance = GetModuleHandleW(NULL);	
+
+	WindowClass.lpszMenuName = NULL;
+
+	WindowClass.lpszClassName = APP_NAME_W L"_WINDOWCLASS";	
+
+	if (RegisterClassExW(&WindowClass) == 0)
+	{
+		Result = GetLastError();
+
+		OutputDebugStringW(L"RegisterClassExW failed!\n");
+
+		goto Exit;
+	}
+
+	HWND TaskbarWindowHandle = FindWindowW(L"Shell_SecondaryTrayWnd", NULL);
+
+	HWND RealTaskbarWindowHandle = FindWindowExW(TaskbarWindowHandle, NULL, L"WorkerW", NULL);
+
+	HWND RealRealTaskbarWindowHandle = FindWindowExW(RealTaskbarWindowHandle, NULL, L"MSTaskListWClass", NULL);
+
+
+	ClockWindowHandle = CreateWindowExW(
+		0,
+		WindowClass.lpszClassName,
+		APP_NAME_W,
+		WS_VISIBLE,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CLOCK_WINDOW_WIDTH,
+		100,
+		RealRealTaskbarWindowHandle,
+		NULL,
+		GetModuleHandleW(NULL),
+		NULL);
+
+	if (ClockWindowHandle == NULL)
+	{
+		Result = GetLastError();
+
+		OutputDebugStringW(L"CreateWindowExW failed!\n");
+
+		goto Exit;
+	}
+
+	// Remove all window styles.
+	SetWindowLongW(ClockWindowHandle, GWL_STYLE, 0);
+
+	// Remove all extended window styles.
+	SetWindowLongW(ClockWindowHandle, GWL_EXSTYLE, 0);
+
+	// Set window style to layered so we can have transparency,
+	// and TOOLWINDOW extended style to hide the window's taskbar icon.
+	SetWindowLongW(
+		ClockWindowHandle, 
+		GWL_EXSTYLE, 
+		GetWindowLongW(ClockWindowHandle, GWL_EXSTYLE) | WS_EX_LAYERED | WS_EX_TOOLWINDOW);
+
+	// Make red pixels transparent.
+	SetLayeredWindowAttributes(ClockWindowHandle, RGB(255, 0, 0), 0, LWA_COLORKEY);
+	
+
+	// Now we must determine where to place this window. It goes on the taskbar of the non-primary
+	// monitor. How do we know which monitor is primary and which monitor is a secondary?
+	// Raymond Chen says: "The primary monitor by definition has its upper left corner at (0, 0)."
+
+	// The taskbar position is relative to the monitor that it's on, so some calculations will need to be
+	// done to figure out the actual absolute coordinates where the clock window will need to be placed.
+
+	EnumDisplayMonitors(NULL, NULL, EnumDisplayProc, (LPARAM)NULL);
+
+	// Since the EnumDisplayMonitors function uses a callback, I assume it is non-blocking,
+	// how to know when all callbacks are complete? What if the user has 24 monitors, it will take
+	// longer than if the user only has 2 monitors. Don't know the best way to handle this.
+	
+	SetWindowPos(
+		ClockWindowHandle, 
+		HWND_TOPMOST,
+		gMonitorInfo.rcMonitor.right - CLOCK_WINDOW_WIDTH - 1, 
+		gMonitorInfo.rcWork.bottom,
+		CLOCK_WINDOW_WIDTH, 
+		48, // Height of the taskbar. TODO: Calculate this smartly
+		0);
+	
+
+	// Make window visible again.
+	SetWindowLongW(ClockWindowHandle, GWL_STYLE, WS_VISIBLE);
+
+	// Message pump must never block or hang or become unresponsive.
+	while (!gShouldExit)
+	{
+		while (PeekMessageW(&WindowMessage, ClockWindowHandle, 0, 0, PM_REMOVE))
+		{
+			DispatchMessageW(&WindowMessage);
+		}
+
+		Sleep(100);
+
+		InvalidateRect(ClockWindowHandle, NULL, TRUE);		
+	}
+
+Exit:
+
+	if (Result != ERROR_SUCCESS)
+	{
+		wchar_t ErrorText[128] = { 0 };
+
+		_snwprintf_s(ErrorText, _countof(ErrorText), _TRUNCATE, L"%s: Error 0x%08lx!", APP_NAME_W, Result);
+
+		MessageBoxW(NULL, ErrorText, L"ERROR", MB_ICONERROR | MB_OK);
+	}
+}
